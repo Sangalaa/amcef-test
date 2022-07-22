ca8bf561f575ee6a71263af65e477f8eb41ff8fb
Adam Bublavy <bublavy.adam@gmail.com>
2022-07-22 14:54:51 +0200

    Add: src/components/todoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
+import { Grid } from '@mui/material';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import { Button } from '@mui/material';
 import Header from './components/header';
 
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <Header />
-      <Typography>Hello World</Typography>
-    </Container>
+      <Container maxWidth="md">
+          <Header />
+          <Typography>Hello World</Typography>
+      </Container>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,12 +2,14 @@ import { AppBar, Toolbar, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import styled from "styled-components";
 
-const Logo = styled(({children, ...props}) => (
-  <Typography variant="h6" component="a" href="/" {...props}>{children}</Typography>
+const Logo = styled(({ children, ...props }) => (
+    <Typography variant="h6" component="a" href="/" {...props}>
+        {children}
+    </Typography>
 ))({
     color: "white",
-    textDecoration: "none"
-})
+    textDecoration: "none",
+});
 
 const Header: React.FC = () => {
     return (
