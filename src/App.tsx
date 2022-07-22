8830b2209fa51d6ab89fe35ed0708e8bd5b7a863
Adam Bublavy <bublavy.adam@gmail.com>
2022-07-22 16:48:00 +0200

    Add: Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { Grid } from "@mui/material";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import Header from "./components/header";
-import { TodoList, todoListActions } from "./store/todoList-slice";
-import { RootState } from "./store";
-import { useEffect } from "react";
-import {default as TodoListComponent} from "./components/todoList";
-import useAxios from "./hooks/useAxios";
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+
+const Home = lazy(() => import("./pages/home"));
 
 export default function App() {
-    const dispatch = useDispatch();
-    const todoLists = useSelector<RootState>((state) => state.todoList.lists) as TodoList[]
-    const { data, loaded, error } = useAxios("items", "GET", null)
-
-    useEffect(() => {
-      if(loaded && data) dispatch(todoListActions.setTodoLists(data))
-    }, [loaded, data, error])
-
     return (
-        <Container maxWidth="md">
-            <Header />
-            <Typography>Hello World</Typography>
-            <Grid container spacing={5} alignItems="center" justifyContent="center">
-                {todoLists.map((list) => (
-                    <Grid item xs={12} sm={6} md={4}>
-                      <TodoListComponent title={list.title} />
-                    </Grid>
-                ))}
-            </Grid>
-        </Container>
+        <BrowserRouter>
+            <Suspense>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
     );
 }
---
+++ src/pages/home.tsx
@@ -0,0 +1,47 @@
+import { Grid } from "@mui/material";
+import { Container } from "@mui/system";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Header from "../components/header";
+import { default as TodoListComponent } from "../components/todoList";
+import useAxios from "../hooks/useAxios";
+import { RootState } from "../store";
+import { TodoList, todoListActions } from "../store/todoList-slice";
+
+const Home: React.FC = () => {
+    const dispatch = useDispatch();
+    const todoLists = useSelector<RootState>(
+        (state) => state.todoList.lists
+    ) as TodoList[];
+    const { data, loaded, error } = useAxios("items", "GET", null);
+
+    useEffect(() => {
+        if (loaded && data) dispatch(todoListActions.setTodoLists(data));
+    }, [loaded, data, error]);
+
+    return (
+        <>
+            <Header />
+
+            <Container maxWidth="lg" sx={(theme) => ({marginTop: theme.spacing(15)})}>
+                <Grid
+                    container
+                    spacing={5}
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    {todoLists.map((todoList) => (
+                        <Grid item xs={12} sm={6} md={4}>
+                            <TodoListComponent
+                                key={todoList.id}
+                                title={todoList.title}
+                            />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+        </>
+    );
+};
+
+export default Home;
