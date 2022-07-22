828df6327cad4fa14668102c465f84c42ebc3d03
Adam Bublavy <bublavy.adam@gmail.com>
2022-07-22 14:09:30 +0200

    Add: src/components/header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import { Button } from '@mui/material';
+import Header from './components/header';
 
 
 export default function App() {
   return (
     <Container maxWidth="sm">
+      <Header />
       <Typography>Hello World</Typography>
     </Container>
   );
---
+++ src/components/header.tsx
@@ -0,0 +1,24 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import styled from "styled-components";
+
+const Logo = styled(({children, ...props}) => (
+  <Typography variant="h6" component="a" href="/" {...props}>{children}</Typography>
+))({
+    color: "white",
+    textDecoration: "none"
+})
+
+const Header: React.FC = () => {
+    return (
+        <AppBar component="nav">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Logo>Todo App</Logo>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
+
+export default Header;
